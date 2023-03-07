2700a8da3820f8c7900a68a5cc15c94cd48a21f5
Cuong-con <127203387+Cuong-con@users.noreply.github.com>
2023-03-07 18:18:45 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Đại Ca Của Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc đại ca của em 8/3 tràn ngập niềm vui và nụ cười. Mong điều đẹp nhất sẽ đến với đại ca trong hôm nay và cả những ngày sau';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
